c7f2f52ee5be90b52f6dafd880b7c677fff73d00
hayakuni <29472187+falconearth@users.noreply.github.com>
2021-12-10 22:43:01 +0900

    Connect screens with react-navigation

--- src/screens/MemoListScreen.jsx
+++ src/screens/MemoListScreen.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 
-import AppBar from '../components/AppBar';
 import CircleButton from '../components/CircleButton';
 import MemoList from '../components/MemoList';
 
-export default function MemolListScreen() {
+export default function MemolListScreen(props) {
+  const { navigation } = props;
   return (
     <View style={styles.container}>
-      <AppBar />
       <MemoList />
-      <CircleButton name="plus" />
+      <CircleButton
+        name="plus"
+        onPress={() => { navigation.navigate('MemoCreate'); }}
+      />
     </View>
   );
 }
