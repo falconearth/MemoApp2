e59af4de5c996db09bacbc84deb461862eecf4c1
hayakuni <29472187+falconearth@users.noreply.github.com>
2021-12-14 22:02:54 +0900

    Minor fix

--- src/screens/MemoEditScreen.jsx
+++ src/screens/MemoEditScreen.jsx
@@ -72,5 +72,8 @@ const styles = StyleSheet.create({
     textAlignVertical: 'top',
     fontSize: 16,
     lineHeight: 24,
+    paddingTop: 32,
+    paddingBottom: 32,
+    paddingHorizontal: 27,
   },
 });
